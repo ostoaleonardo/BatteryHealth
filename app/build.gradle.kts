8d04c3f7588e601c7b27821592bf22f4921ad08c
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-09-16 12:54:22 -0600

    ⚙️ Update build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,14 +12,15 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
