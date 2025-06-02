9d516b25b334580230d1cb0ee9414942fcfdbc28
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2025-06-01 21:10:53 -0600

    🛠️ Add shared preferences util class

---
+++ app/src/main/java/com/monospace/battery/helpers/SharedPreferences.kt
@@ -0,0 +1,25 @@
+package com.monospace.battery.helpers
+
+import android.content.Context
+
+class SharedPreferences(
+    private val context: Context
+) {
+
+    fun getItem(file: String, key: String): String? {
+        return context.getSharedPreferences(file, Context.MODE_PRIVATE)
+            .getString(key, null)
+    }
+
+    fun setItem(file: String, key: String, value: String) {
+        context.getSharedPreferences(file, Context.MODE_PRIVATE)
+            .edit()
+            .putString(key, value)
+            .apply()
+    }
+
+    companion object {
+        const val WIDGETS_FILE = "widgets"
+        const val WIDGETS_PURCHASED = "purchased"
+    }
+}
