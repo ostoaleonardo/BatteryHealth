19e75512ef6d1e1440deb3044013c2d95d7baee7
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2025-06-21 20:13:48 -0600

    🛠️ Update version code to 4 and version name to 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.monospace.battery"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -27,6 +27,9 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            firebaseCrashlytics {
+                nativeSymbolUploadEnabled = true
+            }
         }
     }
     compileOptions {
