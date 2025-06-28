d9856eac0c511bd118ae2b35f9ca6e6ef96c4c4d
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2025-06-28 10:01:22 -0600

    🚀 Update version code to 5 and version name to 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.monospace.battery"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
