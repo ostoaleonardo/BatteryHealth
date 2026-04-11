e328398d8524e0c8f00085a7fb072f5fe9637a51
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2026-04-10 22:51:12 -0600

    🛠️ Remove unused helper and refactor SharedPreferences

--- app/src/main/java/com/monospace/battery/helpers/SharedPreferences.kt
+++ app/src/main/java/com/monospace/battery/helpers/SharedPreferences.kt
@@ -1,21 +1,20 @@
 package com.monospace.battery.helpers
 
 import android.content.Context
+import androidx.core.content.edit
 
 class SharedPreferences(
     private val context: Context
 ) {
 
     fun getItem(file: String, key: String): String? {
-        return context.getSharedPreferences(file, Context.MODE_PRIVATE)
-            .getString(key, null)
+        return context.getSharedPreferences(file, Context.MODE_PRIVATE).getString(key, null)
     }
 
     fun setItem(file: String, key: String, value: String) {
-        context.getSharedPreferences(file, Context.MODE_PRIVATE)
-            .edit()
-            .putString(key, value)
-            .apply()
+        context.getSharedPreferences(file, Context.MODE_PRIVATE).edit {
+            putString(key, value)
+        }
     }
 
     companion object {
