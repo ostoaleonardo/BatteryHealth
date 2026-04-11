d3fbc51179295559bb3f6843f40ce72ec9765754
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2026-04-11 17:44:58 -0600

    🚀 Update version code to 7 and version name to 1.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.monospace.battery"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 7
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
