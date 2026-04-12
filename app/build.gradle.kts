0ecebb506a741bdf3bf8eb1a6e0f9f89c5d29d8b
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2026-04-11 18:20:53 -0600

    🚀 Update version code to 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.monospace.battery"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
+        versionCode = 8
         versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
