d5676f1202fe7ea4273f8324de187653d76732df
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2026-04-11 19:51:01 -0600

    🚀 Update version code to 9 and version name to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.monospace.battery"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
