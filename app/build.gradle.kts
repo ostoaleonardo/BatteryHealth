ee7cb93ae63f72b16a2d9d415b40d9bc5cb993f2
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2026-04-14 21:13:43 -0600

    🚀 Update version code to 11 and version name to 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.monospace.battery"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.1.1"
+        versionCode = 11
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
