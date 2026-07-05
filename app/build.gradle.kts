2cf22e4e5ec6767352fa690769d0fd31b0e4a633
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2026-07-04 22:27:58 -0600

    🚀 Update version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.monospace.battery"
         minSdk = 24
         targetSdk = 37
-        versionCode = 11
-        versionName = "1.1.2"
+        versionCode = 12
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
